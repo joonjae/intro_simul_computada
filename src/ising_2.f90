e9042dd23dbd249c3223a2beef9d4c74cb840755
Sebastian G <sebastian.gossio2010@gmail.com>
2021-10-04 18:17:45 -0300

    agrege la desv estandar y cambios en la formulas

--- src/ising_2.f90
+++ src/ising_2.f90
@@ -5,18 +5,14 @@ program issing_1
 	logical :: es
 	integer :: seed, i, j, nising, niter,nmatriz,n,f,c,f1,c1,M,cont_no_cambia,cont_dE_menor_que_uni,cont_dE_menor_que_cero,E
 	!logical :: parar
-	real:: suma,x, dE, beta, KT,p
-	real, parameter:: Jota=1, Ho=0, K=1, T=5 
+	real:: suma,x, dE, beta, KT,p,Mmedia,Emedia,Mcuadrado,Ecuadrado,desvM,desvE
+	real, parameter:: Jota=1, Ho=0, K=1, T=4
 	!real (kind=8), allocatable :: 
-	integer, allocatable:: S(:,:),Magnetizacion(:),En(:)
+	integer, allocatable:: S(:,:),Mn(:),En(:),aceptado(:)
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11
 !! Revisar formulas 
-!! Agregar un contador para todos los casos 
 !! Agregar el espejo 
-!! Agregar el comentario del paper donde explica bien cada paso 
-!! Agregar el significado de J 
-!!
-!! Analizar que pasa con la temperatura en paramagnetico 
+!!  Agregar el significado de J !!  
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!	 
 ![NO TOCAR] Inicializa generador de número random
 
@@ -57,7 +53,7 @@ KT=K*T
 
 !!!!!
 !!! alocate 
-allocate(S(nmatriz,nmatriz), Magnetizacion(niter),En(niter))
+allocate(S(nmatriz,nmatriz), Mn(niter),En(niter),aceptado(niter))
 !!!!!!!
 
 !!!!!!!!!!!!!!!!!
@@ -74,94 +70,125 @@ do i=1,nmatriz
    end if
    S(i,j)=x 
    !write(16,*) "S(",i,",",j,"):", x 
-   !print *, "i: ", i , "j: ", j, x
-end do 
+   end do 
 end do
 
 do f=10,nising+9 
    write(16,*) (S(f,c),c=10,nising+9)
-   !write(16,fmt="(i5)") (S(f,c), c=1, nising)  
    end do 
 close(16)
 
 cont_no_cambia=0
 cont_dE_menor_que_cero=0
 cont_dE_menor_que_uni=0
-!print *, " " 
-!print *, "Selecciono las filas y columas"
+
 
 write(5,*) "#   n // T[K] // f // c // M // E"
 do n=1,niter
-   !do i=1,nissing
-     ! do j=1,nissing 
         !! lo corro para que no de el 1,1 
         !! cuando lo ponga en forma de toroide usar 1       
         f=nint(uni()*nising)+10 !init redondea 
         c=nint(uni()*nising)+10
-        !print *," "
-        !print *, " Eleccion, fila: ", f, "columna :",c
+        !! cambio un spin en la muestra 
+        S(f,c)=-S(f,c)
         !! calculo el delta E 
-        !dE=-Jota*S(f,c)*(S(f+1,c)+S(f,c+1)+S(f-1,c)+S(f,c-1))-Ho*S(f,c) 
-        dE=2*Jota*S(f,c)*(S(f+1,c)+S(f,c+1)+S(f-1,c)+S(f,c-1))-Ho*S(f,c) !! sacado de matlab
-        !print *, "fila: ", f , "columna: ", c 
-        !print *, "Delta E : ", dE  
+        dE=2*Jota*S(f,c)*(S(f+1,c)+S(f,c+1)+S(f-1,c)+S(f,c-1))-Ho*S(f,c) 
         !! Algoritmo de Markov 
         if (dE<=0)then !! agregar como un and
-          !print *, "delta E <0, cambio de estado"
-          !! cambio el estado 
-          S(f,c)=-S(f,c)
-          cont_dE_menor_que_cero=cont_dE_menor_que_cero+1
-        
+          !! Acepto el nuevo estado  
+          cont_dE_menor_que_cero=cont_dE_menor_que_cero+1        
+          aceptado(n)=1
         else 
-        !! Boltzmann probability of flipping
-        !! prob = exp(-dE / kT);
-          p=exp(-dE/KT)
-          if (p<uni()) then !! no entiendo bien esta parte por que un estado del sistema
-          !print *, "Se p<uni(), cambio de estado"
-          cont_dE_menor_que_uni=cont_dE_menor_que_uni+1
-          S(f,c)=-S(f,c)
+          !! Boltzmann probability of flipping
+          p=exp(-dE/KT) !!Boltzman
+          if (uni()<p) then  
+              !! acepto el nuevo estado 
+              cont_dE_menor_que_uni=cont_dE_menor_que_uni+1
+              aceptado(n)=1 
           else 
-          !print *, "No se cambia el spin"
-          cont_no_cambia=cont_no_cambia+1
-          end if
-              
+             !! no acepto el nuevo estado 
+             !! vuelvo al anterior
+             cont_no_cambia=cont_no_cambia+1
+             aceptado(n)=0
+             S(f,c)=-S(f,c)
+             dE=0
+          end if               
         end if  
+        
+        if (mod(n,1000)==0) then   
+        do f=10,nising+9
+          write(7,*) (S(f,c),c=10,nising+9)  
+        end do    
+        end if                
+       
           M=0
           E=0
+          if (n==1) then 
           !! esta desde 10 porque esta corrida
-          do f1=10,nising+9
-            do c1=10,nising+9
-              M=M+S(f1,c1)
-              E=E+S(f1,c1)*(S(f1+1,c1)+S(f1,c1+1)+S(f1-1,c1)+S(f1,c1-1))-Ho*S(f1,c1)      
-              !print *," fila: ",f1, "Columna", c1 
-            end do 
-         end do 
-         Magnetizacion(n)=M
-         En(n)=E      
-         write(5,fmt="(i7,x,f10.3,x,i5,x,i5,x,x,i5,x,x,i5)") n, T,f,c, Magnetizacion(n), En(n)
-  
-
+            do f1=10,nising+9
+              do c1=10,nising+9
+                !! es el Hamiltoniano
+                En(1)=E+-J*S(f1,c1)*(S(f1+1,c1)+S(f1,c1+1)+S(f1-1,c1)+S(f1,c1-1))-Ho*S(f1,c1)      
+                Mn(1)=M+S(f1,c1)
+              end do 
+            end do         
+          else 
+               En(n)=dE+En(n-1)            
+               Mn(n)=2*S(f,c)+Mn(n-1)
+          end if             
+            
+         write(5,fmt="(i7,x,f10.3,x,i5,x,x,i5,x,x,i5,x,x,i5,x,x,i5)") n, T,f,c,aceptado(n), Mn(n), En(n)
 end do 
-
 close(5)
 
-do f=10,nising+9
-   write(7,*) (S(f,c),c=10,nising+9)  
-end do 
-close(7)
+        do f=10,nising+9
+          write(7,*) (S(f,c),c=10,nising+9)  
+        end do 
+        close(7)
 
+Mmedia=0
+Emedia=0
+Ecuadrado=0
+Mcuadrado=0
+do i=1,niter
+    Emedia=Emedia+En(i)        
+    Mmedia=Mmedia+Mn(i)      
+    Mcuadrado=Mcuadrado+Mn(i)**2
+    Ecuadrado=Ecuadrado+En(i)**2 
+end do 
+
+Emedia=Emedia/dble(niter*nising**2)
+Mmedia=Mmedia/dble(niter*nising**2)
+Mcuadrado=Mcuadrado/dble(niter*nising**2)
+Ecuadrado=Ecuadrado/dble(niter*nising**2)
+
+desvE=(Ecuadrado-Emedia**2)**0.5
+desvM=(Mcuadrado-Mmedia)**0.5
 
 print *, " " 
 print *, "Temperatura [K]:", T 
 print *, " "
 print '(" dE<0  %:" f10.4)', 100*dble(cont_dE_menor_que_cero)/dble(niter)
 print *, " "
-print '(" p<uni() %:" f10.4)',100*dble(cont_dE_menor_que_uni)/dble(niter)
-print *, " "
+print '(" uni()<p %:" f10.4)',100*dble(cont_dE_menor_que_uni)/dble(niter)
+print *, " --------------------"
 print  '(" Cambia %:", f10.4)', 100*dble(niter-cont_no_cambia)/dble(niter)
 print *, " "
 print  '(" No cambia %:" f10.4)', 100*dble(cont_no_cambia)/dble(niter)
+print *, " --------------------"
 print *, " " 
+print '(" Energia Media por Spin:" f10.5)', Emedia
+!print *, " "
+!print '(" Energia cuadrado por Spin" f10.4)', Ecuadrado
+print *, " " 
+print '(" Desviacion de E:" f10.4)',desvE
+print *, " "   
+print '(" M Media por Spin:" f10.4)', Mmedia
+print *," " 
+!print '(" M cuadrado por Spin" f10.4)', Mcuadrado
+!print *, " " 
+print '(" Desviacion de M: " f10.4)', desvM
+print *, " --------------------"
 print *, "Fin del programa"
 print *, " "  
 
