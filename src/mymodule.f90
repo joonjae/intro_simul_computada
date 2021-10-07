673aff6ec2b978baa0a6ca0f0791f5b83cfc39f5
joonjae <leecartas@hotmail.com>
2021-10-06 23:47:23 -0300

    agrego subrutinas y pgplot para mostrar

--- src/mymodule.f90
+++ src/mymodule.f90
@@ -4,11 +4,42 @@ MODULE mymodule
 
       PRIVATE
 
-      PUBLIC :: funcion_x, init_mapa, rpos_mapa
+      !PUBLIC :: funcion_x, init_mapa, rpos_mapa
+      PUBLIC :: funcion_x, init_mapa, rand_pos, plot_mapa
 
 CONTAINS
 
+! Subrutina para mostrar el mapa
+! Para llamar esta subrutina se debe haber iniciado el pgplot
+! (IN)  ier: referencia de pgbeg()
+! (IN)  mapa: matriz unos y ceros que representa el mapa
+! (IN)  nsize: tamanio de la matriz (mapa). ej. nsize=3 => 3 x 3
+subroutine plot_mapa(ier,mapa,nsize)
+    implicit none
+    integer j,i
+    integer, intent(in) :: ier
+    integer, intent(in) :: nsize
+    integer, intent(in) :: mapa(nsize,nsize)
+
+    do j=1,nsize
+        do i=1,nsize
+            if(mapa(i,j)==1) then
+                !call pgsfs(1) ! circulo relleno
+                !call pgsci(3) ! verde
+                call pgsci(2) ! rojo
+            else
+                !call pgsfs(2) ! circulo con contorno
+                call pgsci(1) ! blanco
+            end if
+            call pgcirc(real(i),real(j),0.25)
+        end do
+    end do 
+    return
+end subroutine 
+
 ! Subrutina para crear la matriz 
+! (IN)  mapa: escribe aleatoriamente en esta matriz unos y ceros
+! (OUT) n: es el tamanio del mapa
 subroutine init_mapa(mapa,n)
     implicit none
     integer j,i
@@ -20,6 +51,7 @@ subroutine init_mapa(mapa,n)
             if(uni()<0.5)then
                 mapa(i,j)=  1
             else
+                !mapa(i,j)= 0
                 mapa(i,j)= -1
             end if
         end do
@@ -27,30 +59,53 @@ subroutine init_mapa(mapa,n)
     return  
 end subroutine init_mapa
 
-! Selecciona una posicion random de la matriz de mapa
-subroutine rpos_mapa(mapa,n,pos,npos)
+! Subrutina que elige una posicion random dentro de la matriz del mapa
+! (IN)  nsize: tamanio de la matriz (mapa). ej. nsize=3 => 3 x 3
+! (IN)  ndim:  la dimension de la matriz (mapa). ej. ndim=3 => 3 x 3 x 3
+! (OUT) rpos:  retorna una posicion de la matriz elegida aleatoriamente
+subroutine rand_pos(nsize,ndim,rpos)
     implicit none
     integer i
-    real rvar
-    integer, intent(in) :: n, npos  ! n: tamanio del mapa n x n
-                                    ! npos: dimension de la matriz (n x n => 2)
-    integer, intent(in) :: mapa(n,n)
-    integer, intent(out):: pos(0:npos-1)
+    integer rvar
+    integer, intent(in) :: nsize, ndim
+    integer, intent(out) :: rpos(0:ndim-1)
 
     i = 0
-    do while (i<npos)
+    do while (i<ndim)
         rvar = abs(shr3())
-        !if( 0<=rvar .and. rvar<=n )then
-        if( rvar<=n )then
-            pos(i) = rvar
+        !if( 0<rvar .and. rvar<=nsize )then
+        if( rvar<=nsize )then
+            rpos(i) = rvar
             i = i + 1
         end if
     end do
     return
-end subroutine rpos_mapa
+
+end subroutine rand_pos
+!subroutine rpos_mapa(mapa,n,npos,pos)
+!    implicit none
+!    integer i
+!    real rvar
+!    integer, intent(in) :: n, npos  ! n: tamanio del mapa n x n
+!                                    ! npos: dimension de la matriz (n x n => 2)
+!    integer, intent(in) :: mapa(n,n)
+!    integer, intent(out):: pos(0:npos-1)
+!
+!
+!    i = 0
+!    do while (i<npos)
+!        rvar = abs(shr3())
+!        !if( 0<=rvar .and. rvar<=n )then
+!        if( rvar<=n )then
+!            pos(i) = rvar
+!            i = i + 1
+!        end if
+!    end do
+!    return
+!end subroutine rpos_mapa
 
 
-
+! Ejercicio
 FUNCTION funcion_x( x , y ) RESULT( hits )
       real :: x , y , fdx
       integer :: hits
--- src/problema_ising.f90
+++ src/problema_ising.f90
@@ -9,12 +9,23 @@ program problema_ising
 
     ! Variables
     logical :: es
-    integer :: seed, i , j
+    integer :: seed, i , j, icolor
+    integer :: L = 50 ! tamanio de la matriz. ej. L x L
     integer , allocatable :: mapa(:,:)
-    integer :: L = 6, dim_mapa
     integer , allocatable :: init_pos(:)
+    integer :: dim_mapa
 
-    ! Inicio generador d nro random
+    integer :: ier,pgbeg ! para referenciar al inicio de pgplot
+    integer, allocatable :: ip(:),im(:) ! para ir mapeando cuales son los vecinos
+    real :: N0, U, T, H
+    integer :: N_MC
+    integer :: imc, i_in, N_INTER, i_area, area
+    integer :: isum
+    real :: energia_antigua, energia_nueva, delta_energia, ising
+
+    real :: ht, ut
+    real :: densidad, pasos
+    ! Inicio generador nro random
     inquire(file='seed.dat',exist=es)
     if(es) then
         open(unit=10,file='seed.dat',status='old')
@@ -27,18 +38,98 @@ program problema_ising
     call zigset(seed)
     !----------------------------------------------------
 
+    ! Preparo el pgplot
+    ier = pgbeg(0,'/xserve',1,1)
+    if(ier .ne. 1) STOP
+    call pgenv(0,real(L+1),0,real(L+1),1,0)
+    !----------------------------------------------------
 
-    allocate(mapa(L,L))
-    dim_mapa = size(shape(mapa))
-    allocate(init_pos(dim_mapa))
+    ! Inicio lectura de input para el ising
+    ! N0: nro de spin
+    ! U: Energia de interaccion
+    ! T: Temperatura
+    ! H: Campo magnetico
+    ! N_MC: nro de iteraciones de Monte Carlo
+    inquire(file='input_ising.dat',exist=es)
+    if(es) then
+        open(unit=1,file='input_ising.dat',status='old')
+        read(1,*) N0 ,U ,T, H
+        read(1,*) N_MC, N_INTER
+        close(1)
+        print *,"  * leyendo datos de input_ising.dat"
+    else
+        N0=0
+        U=1
+        T=1
+        H=0
+        N_MC=1000
+    end if
+    close(1)
+    print *,"N0:",N0," U:",U ,"T:",T ,"H",H, "N_MC:",N_MC
 
-    call init_mapa(mapa,L)
-    do j=1,L
-        write(*,*) (mapa(i,j), i=1,L)
+    !----------------------------------------------------
+
+
+
+    allocate(mapa(L,L))             ! reservo memoria para el mapa
+    dim_mapa = size(shape(mapa))    ! verifico la dimension
+    allocate(init_pos(dim_mapa))    ! reservo memoria para la posicion inicial dentro del mapa
+
+    call init_mapa(mapa,L)          ! inicio el mapa 
+    !do j=1,L                            
+    !    write(*,*) (mapa(i,j), i=1,L)   ! muestro por pantalla el mapa
+    !end do
+
+    call plot_mapa(ier,mapa,L)
+
+    call rand_pos(L,dim_mapa,init_pos)      ! pido una posicion aleatoria en el "init_pos"
+    print *,init_pos                    ! muestro por pantalla "init_pos"
+
+
+    ut = U/T
+    ht = H/T
+
+    area = L*L
+    densidad = 1./real(area)
+    pasos = 1./real(N_MC)
+
+    
+    allocate(ip(L))             ! reservo memoria para suma vecinos
+    allocate(im(L))             ! reservo memoria para restar vecinos
+
+    do i=1, L
+        ip(i) = i+1
+        im(i) = i-1
     end do
+    ip(L) = 1
+    im(1) = L
 
-    call rpos_mapa(mapa,L,init_pos,dim_mapa)
-    print *,init_pos
+    do imc=1, N_MC
+        do i_in=1, N_INTER
+            do i_area=1, area
+                i = int(L*uni()*0.99999999999999999) + 1
+                j = int(L*uni()*0.99999999999999999) + 1
+                isum = mapa(im(i),j)+mapa(ip(i),j)+mapa(i,im(j))+mapa(i,ip(j))
+                energia_antigua = -ut*mapa(i,j)*isum - ht
+                energia_nueva   =  ut*mapa(i,j)*isum + ht
+                delta_energia   =  exp(energia_antigua - energia_nueva)
+                if(uni() < delta_energia)then
+                    ising = (mapa(i,j)+3)/2
+                    call pgsci(0)
+                    call pgsfs(ising)
+                    call pgcirc(real(i),real(j),1./4.)
+                    mapa(i,j) = -mapa(i,j)
+                    ising = (mapa(i,j)+3)/2
+                    call pgsci(ising)
+                    call pgsfs(ising)
+                    call pgcirc(real(i),real(j),1./4.)
+                    print *,"acepta"
+                !else
+                    !print *,"no acepta"
+                end if
+            end do
+        end do
+    end do
 
     ! Escribir la última semilla para continuar con la cadena de numeros aleatorios 
     open(unit=10,file='seed.dat',status='unknown')
